e2c4e00983702033d935f57abedfcf4111af7b30
gtrentz <grant-trentz@uiowa.edu>
2023-12-19 21:48:31 -0600

    main

---
+++ app/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Workout App</h1>
+      <p>Get ready to get fit!</p>
+      <button>Start Workout</button>
+    </div>
+  );
+};
+
+//Commit test
+//main
+export default Home;
